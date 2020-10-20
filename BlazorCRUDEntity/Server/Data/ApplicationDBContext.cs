0104a9475fd88da1653f798914eb030292941276
Tmham92 <tmham92@gmail.com>
2020-10-20 13:33:48 +0200

    Add project files.

---
+++ BlazorCRUDEntity/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorCRUDEntity.Shared.Models;
+
+namespace BlazorCRUDEntity.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Developer>().Property(d => d.Experience).HasColumnType("DECIMAL(5,2)");
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
---
+++ BlazorCRUDEntity/Server/Migrations/20201020092832_initial2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorCRUDEntity.Server.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Experience",
+                table: "Developers",
+                type: "DECIMAL(5,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Experience",
+                table: "Developers",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(5,2)");
+        }
+    }
+}
